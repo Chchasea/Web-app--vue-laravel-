fa641431d12fa84c01c79bdf931e56db90e9d8f0
GuilerMarionR <156042025+GuilerMarionR@users.noreply.github.com>
2024-05-09 14:40:37 +0800

    adding answer.vue

---
+++ resources/js/Pages/Answer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import Layout from "@/Pages/Layout.vue";
+import "@mdi/font/css/materialdesignicons.css";
+</script>
+
+<script>
+export default {
+    data: () => ({
+        rating: 4.5,
+    }),
+};
+</script>
